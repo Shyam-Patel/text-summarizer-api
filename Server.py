f44775a1c8f374d7b81ed2f4a9e8cdf9d756181c
Shyam Patel <shyam.p95@gmail.com>
2019-11-26 17:23:47 -0500

    Refactor server.py

--- Server.py
+++ Server.py
@@ -5,14 +5,11 @@ from Blueprints.SumySummarize import SumySummarize_Blueprint
 
 app = Flask(__name__)
 
+# Enables cross-origin API requests
+CORS(app)
 
-@app.route("/")
-def setup():
-    # Enables cross-origin API requests
-    CORS(app)
+# Register all the blueprints (API endpoints)
+app.register_blueprint(GensimSummarize_Blueprint, url_prefix="/gensim")
+app.register_blueprint(SumySummarize_Blueprint, url_prefix="/sumy")
 
-    # Register all the blueprints (API endpoints)
-    app.register_blueprint(GensimSummarize_Blueprint, url_prefix="/gensim")
-    app.register_blueprint(SumySummarize_Blueprint, url_prefix="/sumy")
-
-    app.run()
+app.run()
